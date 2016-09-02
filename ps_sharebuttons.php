db0825a9964abde59312a1bfb6631131c7604d08
Maxime Biloé <maxime.biloe@prestashop.com>
2016-09-02 11:18:10 +0200

    // Version updated to v1.0.1

--- ps_sharebuttons.php
+++ ps_sharebuttons.php
@@ -40,7 +40,7 @@ class Ps_Sharebuttons extends Module implements WidgetInterface
         $this->name = 'ps_sharebuttons';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
         $this->bootstrap = true;
         $this->_directory = dirname(__FILE__);
